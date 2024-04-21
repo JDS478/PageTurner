ed47d66af56c13c73ef38dfff2216e3f3857307f
Joel Savage <joel28169@gmail.com>
2024-04-21 17:03:35 +0100

    Migrated books model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_21_155748) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_21_160110) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,4 +21,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_21_155748) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "books", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.string "title"
+    t.text "description"
+    t.datetime "publication_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  add_foreign_key "books", "authors"
 end
